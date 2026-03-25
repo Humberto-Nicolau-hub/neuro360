f5e7326df4001c9c5d8513df877a58c5e50486d8
Humberto-Nicolau-hub <contatobetaoofertas@gmail.com>
2026-03-25 15:23:23 -0300

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>NeuroMapa360</h1>
+      <p>Seu projeto está funcionando 🚀</p>
+    </div>
+  );
+}
+
+export default App;

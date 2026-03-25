9dd1ff178518c2942f8d225a01017665bf5dab9b
Humberto-Nicolau-hub <contatobetaoofertas@gmail.com>
2026-03-25 15:47:11 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div>
       <h1>NeuroMapa360</h1>
-      <p>Seu projeto está funcionando 🚀</p>
     </div>
   );
 }

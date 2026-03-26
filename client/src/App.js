706ce5e26c85e89c17276eea4f8d438d223b45f8
Humberto-Nicolau-hub <contatobetaoofertas@gmail.com>
2026-03-26 15:08:52 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { supabase } from "./supabase";
 
 export default function App() {
+  const [dados, setDados] = useState([]);
 
   async function salvarDados() {
-    const { data, error } = await supabase
-      .from("feedbacks")
-      .insert([
-        {
-          usuario: "Humberto",
-          trilha: "Ansiedade",
-          eficaz: true,
-          comentario: "Teste funcionando"
-        }
-      ]);
+    await supabase.from("feedbacks").insert([
+      {
+        usuario: "Humberto",
+        trilha: "Ansiedade",
+        eficaz: true,
+        comentario: "Teste funcionando"
+      }
+    ]);
 
-    if (error) {
-      alert("Erro ao salvar!");
-      console.log(error);
-    } else {
-      alert("Salvo com sucesso!");
-      console.log(data);
-    }
+    buscarDados();
   }
+
+  async function buscarDados() {
+    const { data } = await supabase.from("feedbacks").select("*");
+    setDados(data);
+  }
+
+  useEffect(() => {
+    buscarDados();
+  }, []);
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🚀 NeuroMapa360</h1>
 
       <button onClick={salvarDados}>
-        Testar conexão com banco
+        Salvar novo feedback
       </button>
+
+      <h2>📊 Feedbacks:</h2>
+
+      {dados.map((item, index) => (
+        <div key={index}>
+          <p>
+            {item.usuario} - {item.trilha} - {item.comentario}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }

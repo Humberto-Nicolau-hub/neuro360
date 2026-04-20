ac33e9ae1dc1b55fef26376e8032acb9e1d46aab
Humberto-Nicolau-hub <contatobetaoofertas@gmail.com>
2026-04-20 17:58:59 -0300

    Update EvolucaoChart.js

--- client/EvolucaoChart.js
+++ client/EvolucaoChart.js
@@ -1,12 +1,22 @@
-import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer
+} from "recharts";
 
 export default function EvolucaoChart({ data }) {
   return (
-    <LineChart width={350} height={200} data={data}>
-      <XAxis dataKey="data" />
-      <YAxis domain={[1, 5]} />
-      <Tooltip />
-      <Line type="monotone" dataKey="valor" stroke="#22c55e" />
-    </LineChart>
+    <ResponsiveContainer width="100%" height={250}>
+      <LineChart data={data}>
+        <XAxis dataKey="data" />
+        <YAxis />
+        <Tooltip />
+        <Line type="monotone" dataKey="valor" />
+      </LineChart>
+    </ResponsiveContainer>
   );
 }
